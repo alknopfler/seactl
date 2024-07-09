eecdebbd376dda82c1853c56d4697a3b4bf8cb37
Alberto Morgante Medina <alberto.morgante@suse.com>
2024-07-09 12:34:50 +0200

    Add config cmd structure

---
+++ cmd/airgap.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	releaseManifestFile string
+	registryUsername    string
+	registryPassword    string
+	registryURL         string
+	registryCACert      string
+	outputTarball       string
+)
+
+func NewAirGapCommand() *cobra.Command {
+
+	c := &cobra.Command{
+		Use:   "generate",
+		Short: "Command to generate the air-gap artifacts from the release manifest",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+	// Add flags
+	flags := c.Flags()
+	flags.StringVarP(&releaseManifestFile, "input", "i", "", "Release manifest file")
+	flags.StringVarP(&registryUsername, "registry-username", "u", "", "Registry Username")
+	flags.StringVarP(&registryPassword, "registry-password", "p", "", "Registry Password")
+	flags.StringVarP(&registryURL, "registry-url", "r", "", "Registry URL")
+	flags.StringVarP(&registryCACert, "registry-cacert", "c", "", "Registry CA Certificate file")
+	flags.StringVarP(&outputTarball, "output", "o", "", "Output tarball file")
+	// add options and required flags
+	c.MarkFlagRequired("input")
+	c.MarkFlagsOneRequired("output", "registry-url")
+	c.MarkFlagsMutuallyExclusive("output", "registry-url")
+	return c
+}
--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func newCommand() *cobra.Command {
 		Short: "SUSE Edge Air-gap tool enables to create an air-gap scenario using the suse-edge release manifest",
 		Long: "SUSE Edge Air-gap tool enables to create an air-gap scenario using the suse-edge release manifest. The output could be a tarball, but also you could upload to a private registry.\n" +
 			"Features: \n" +
-			"- Read the SUSE Edge release manifest (from --input command as well as from any URL\n" +
+			"- Read the SUSE Edge release manifest (from input file)\n" +
 			"- Save artifacts to a tarball\n" +
 			"- Login to a private registry\n" +
 			"- Upload and preload the private registry with the artifcats\n",
@@ -35,7 +35,7 @@ func newCommand() *cobra.Command {
 		},
 	}
 
-	c.AddCommand(cmd.NewRegistryLoginCommand())
+	c.AddCommand(cmd.NewAirGapCommand())
 
 	return c
 }
